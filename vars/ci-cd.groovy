c1e8aa71a09109faa2e0ef0f16391bc40d6c1d22
mouli <168059375+MouliTechHub@users.noreply.github.com>
2024-05-27 11:36:43 +0530

    Update ci-cd.groovy

--- vars/ci-cd.groovy
+++ vars/ci-cd.groovy
@@ -1,5 +1,5 @@
 def maven(){
   def mavenHome = tool name: "Maven-3.9.6", type: "maven"
-        def mavenCMD = "${mavenHome}/bin/mvn"
-        sh "${mavenCMD} clean package"
+  def mavenCMD = "${mavenHome}/bin/mvn"
+  sh "${mavenCMD} clean package"
 }
